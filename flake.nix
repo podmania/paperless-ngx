464411974814f800244c2958c6d83290a1d9833f
bryce <bryce.hoehn@icloud.com>
2026-05-09 16:20:09 -0400

    feat: add debug image variant with shared base-debug layer

--- flake.nix
+++ flake.nix
@@ -11,25 +11,35 @@
     system = builtins.currentSystem;
     pkgs = nixpkgs.legacyPackages.${system};
     n2c = nix2container.outputs.packages.${system}.nix2container;
+    imageConfig = {
+      ExposedPorts = {
+        "8000/tcp" = {};
+      };
+      Volumes = {
+        "/data" = {};
+        "/media" = {};
+        "/export" = {};
+        "/consume" = {};
+      };
+      Cmd = [ "${pkgs.paperless-ngx}/bin/paperless-ngx" ];
+    };
   in {
     packages.${system} = {
       paperless-ngx-image = n2c.buildImage {
         name = "paperless-ngx";
         tag = "latest";
         fromImage = base.packages.${system}.base-image;
-        config = {
-          ExposedPorts = {
-            "8000/tcp" = {};
-          };
-          Volumes = {
-            "/data" = {};
-            "/media" = {};
-            "/export" = {};
-            "/consume" = {};
-          };
-          Cmd = [ "${pkgs.paperless-ngx}/bin/paperless-ngx" ];
-        };
+        config = imageConfig;
       };
+
+      paperless-ngx-debug-image = n2c.buildImage {
+        name = "paperless-ngx";
+        tag = "latest-debug";
+        fromImage = base.packages.${system}.base-debug-image;
+        config = imageConfig;
+      };
+
+      paperless-ngx = pkgs.paperless-ngx;
 
       default = self.packages.${system}.paperless-ngx-image;
     };
